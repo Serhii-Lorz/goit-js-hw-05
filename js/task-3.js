f4546ffed5f7be5e98e5a04c25db9e46dec68b19
Serhii Yehorov <egorovs228228@gmail.com>
2024-12-11 16:16:21 +0000

    fix task-3,js file

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const sortByDescendingFriendCount = (users) =>
-  [...users].sort((a, b) => b.friends.length - a.friends.length);
+  users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 
 console.log(
